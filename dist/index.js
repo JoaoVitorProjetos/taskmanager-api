291f403044255b7c07fe5485c7f0bdadc4e11cd9
Joao <joaovitorcomercial06@gmail.com>
2023-09-13 20:01:07 -0300

    Tentando configurar o cors mais uma vez

--- dist/index.js
+++ dist/index.js
@@ -6,12 +6,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const cors_1 = __importDefault(require("cors"));
+const options = {
+    allowedHeaders: [
+        'Origin',
+        'X-Requested-With',
+        'Content-Type',
+        'Accept',
+        'X-Access-Token',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: 'https://crud-typescript-frontend.vercel.app',
+    preflightContinue: false,
+};
 const routes_1 = __importDefault(require("./routes"));
 mongoose_1.default.connect(process.env.DATABASE_URI || 'mongodb+srv://root:Joaovitor123@databse-joao.voiwao9.mongodb.net/?retryWrites=true&w=majority');
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)(options));
+app.options('*', (0, cors_1.default)(options));
 const port = process.env.PORT || 3003;
 app.use(routes_1.default);
 app.listen(port, () => { console.log(`Funcionando na porta ${port}`); });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import cors from 'cors'
+import cors from 'cors';
+
+const options: cors.CorsOptions = {
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'X-Access-Token',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: 'https://crud-typescript-frontend.vercel.app',
+    preflightContinue: false,
+  };
 
 import routes from './routes'
 
@@ -9,7 +23,9 @@ mongoose.connect(process.env.DATABASE_URI || 'mongodb+srv://root:Joaovitor123@da
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cors());
+app.use(cors(options));
+app.options('*', cors(options));
+
 
 const port = process.env.PORT || 3003;
 
