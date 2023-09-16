354d81549aa5f205ab2cc430c64a557f261d2424
Joao <joaovitorcomercial06@gmail.com>
2023-09-15 22:53:04 -0300

    ??

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const options: cors.CorsOptions = {
     ],
     credentials: true,
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: 'http://localhost:3003',
+    origin: 'http://localhost:3000',
     preflightContinue: false,
   };
 
